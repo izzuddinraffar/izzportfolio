7cd196a78371b7e613d082d8665ffeb0c0ca413d
Mohamad Izzuddin <izzuddinraffar@gmail.com>
2019-11-10 21:47:27 +0800

    added dist folder

---
+++ dist/precache-manifest.88188d52aedff059e507dbef97e40567.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9bd6542a460d8470bc",
+    "url": "/css/app.26a93ee6.css"
+  },
+  {
+    "revision": "4a90388b7ae4c86f51d0",
+    "url": "/css/chunk-vendors.3640432b.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "26618f30e161326378951f226010bed5",
+    "url": "/img/avatar.26618f30.jpg"
+  },
+  {
+    "revision": "f456f5b247540085c53f53f72eea246f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e682ee9e56ffae82fea",
+    "url": "/js/about.854e429d.js"
+  },
+  {
+    "revision": "fe9bd6542a460d8470bc",
+    "url": "/js/app.3bf31f0b.js"
+  },
+  {
+    "revision": "4a90388b7ae4c86f51d0",
+    "url": "/js/chunk-vendors.df0dfdac.js"
+  },
+  {
+    "revision": "017b5d3dd8ae66da3519f8cae5b06931",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
