c4595f33c00a63b72af234cb3c5c85214c0faaf2
Mohamad Izzuddin <izzuddinraffar@gmail.com>
2019-11-14 10:30:39 +0800

    contact page added

---
+++ src/views/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container pa-0">
+    <div class="header"></div>
+  </div>
+</template>
+
+
+
+<style lang="scss">
+  .container{
+    .header{
+       height:137px;
+       width:100%;
+       background-color:#007791;
+    }
+  }
+</style>
